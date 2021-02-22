c0aaca1e7f2d9d60c428f9cce784ddb4aed4017d
Anna Starkova <annastarkova2018@gmail.com>
2021-02-22 20:31:26 +0500

    added test

--- src/test/java/ru/netology/stats/StatisticsServiceTest.java
+++ src/test/java/ru/netology/stats/StatisticsServiceTest.java
@@ -9,7 +9,7 @@ class StatisticsServiceTest {
     @Test
     void findMax() {
         StatisticsService service = new StatisticsService();
-        long[] incomesInBillions = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
 
         // вызываем целевой метод:
